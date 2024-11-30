18936ef34e9adf25cb71d75e20e928b08a7dae5e
Adarsh Sharma <69235560+AdarshSharma-97@users.noreply.github.com>
2024-11-30 15:33:15 +0530

    [50] [Time Beats: 5.06%] [Memory Beats: 72.38%] - LeetPush

--- 50-Pow(x,-n).cpp
+++ 50-Pow(x,-n).cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     double myPow(double x, int n) {
+        double myPow(double x, int n) {
         if(n==0) return 1.0;
         if(x==0) return 0.0;
         if(x==1) return 1.0;
